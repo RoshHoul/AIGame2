ffca97b9babbbb84f3fd11a13c4970d8c6c4cce1
RoshHoul <kabadzhg@uni.coventry.ac.uk>
2019-03-22 01:03:42 +0000

    Added Walking state to the AI, pre terrain modifications

---
+++ Georgi Kabadzhov 2nd AI Game/Assets/Scripts/AICrowdManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AICrowdManager : MonoBehaviour
+{
+
+    List<Transform> Waypoints = new List<Transform>();
+    public List<Transform> SpawnPoints = new List<Transform>();
+    public List<Transform> EntryPoints = new List<Transform>();
+    public List<Peasant> Humans = new List<Peasant>();
+
+    public GameObject humanPrefab;
+
+    public int populationCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject waypointsHolder = GameObject.FindGameObjectWithTag("TrafficWaypoints");
+
+        foreach (Transform child in waypointsHolder.transform) {
+            Waypoints.Add(child);
+            if (child.gameObject.tag.Equals("SpawnPoints")) {
+                SpawnPoints.Add(child);
+            } else if (child.gameObject.tag.Equals("EntryPoints")) {
+                EntryPoints.Add(child);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Humans.Count <= populationCount) {
+            Debug.Log("IN IF " + Humans.Count) ;
+            //SpawnHuman();
+            GameObject human = Instantiate(humanPrefab, SpawnPoints[0].position, transform.rotation);
+            Humans.Add(human.GetComponent<Peasant>());
+            Humans[Humans.Count - 1].SetGoal(EntryPoints[0]);
+            Humans[Humans.Count - 1].previousGoal = SpawnPoints[0];
+
+            
+        }        
+    }
+
+    void SpawnHuman() {
+        int randomStart = RandomGenerator();
+        GameObject human = Instantiate(humanPrefab, SpawnPoints[randomStart].position, transform.rotation);
+        //human.GetComponent<Peasant>().SetGoal(EntryPoints[randomStart]);
+//        Humans.Add(human);
+        
+    }
+
+
+    //The first and the last two elements of waypoints are entries or spawns. Remove the magic numbers (todo:)
+    public Transform GoalRequest(Transform prevGoal) {
+        if (SpawnPoints.Contains(prevGoal) || EntryPoints.Contains(prevGoal)) {
+            return Waypoints[Random.Range(2, Waypoints.Count - 3)];
+
+        }
+        return Waypoints[Random.Range(0, Waypoints.Count-1)];
+    }
+
+    int RandomGenerator() {
+        return Random.Range(0, 100) % 2; 
+    }
+}
--- Georgi Kabadzhov 2nd AI Game/Assets/Scripts/Peasant.cs
+++ Georgi Kabadzhov 2nd AI Game/Assets/Scripts/Peasant.cs
@@ -6,19 +6,57 @@ using UnityEngine.AI;
 public class Peasant : MonoBehaviour
 {
 
-    public GameObject goal;
+    public Transform goal;
+    public Transform previousGoal;
     NavMeshAgent agent;
+    AICrowdManager crowdManager;
+
+    public enum State {
+        IDLE,
+        WALKING,
+    }
+
+    public State state;    
 
     // Start is called before the first frame update
     void Start()
     {
         agent = this.GetComponent<NavMeshAgent>();
+        crowdManager = GameObject.Find("CrowdManager").GetComponent<AICrowdManager>();
+        state = State.WALKING;
+        
+        if (goal == null) {
+            goal = crowdManager.GoalRequest(null);
+        }
+        //goal = crowdManager.GoalRequest(previousGoal);
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(goal.transform.position);        
-        
+        if(state == State.WALKING) {
+            if (goal == null) {
+                goal = crowdManager.GoalRequest(previousGoal);    
+            }
+            agent.SetDestination(goal.transform.position);        
+            
+            if (agent.remainingDistance <= agent.stoppingDistance) {
+                if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f) {
+                    previousGoal = goal;
+                    if (previousGoal.gameObject.tag == "EntryPoints" || previousGoal.gameObject.tag == "SpawnPoints") {
+                        goal = crowdManager.GoalRequest(previousGoal);
+                    } else {
+                        state = State.IDLE;
+                    }
+                }
+            }    
+        } 
+        else if (state == State.IDLE) {
+            agent.isStopped = true;
+        }
+    }
+
+    public void SetGoal(Transform newGoal) {
+        goal = newGoal;
     }
 }
